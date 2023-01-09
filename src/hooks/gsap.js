9a0ef84525128b457e4ed12f37bc1ff4b1ad5b05
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-09 23:15:31 +0600

    projects drafted

--- src/hooks/gsap.js
+++ src/hooks/gsap.js
@@ -1,5 +1,8 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const useLinkDownfall = (arr) => {
   useEffect(() => {
@@ -78,4 +81,102 @@ export const useBoxScaling = (arr) => {
       }
     );
   }, [arr]);
+};
+
+export const useProjectTitleSpan = (arr, trig) => {
+  useEffect(() => {
+    const el = arr.map((item) => item.current);
+    const trigEl = trig.current;
+
+    gsap.fromTo(
+      el,
+      {
+        y: -500,
+      },
+      {
+        y: 0,
+        duration: 1,
+        ease: "power4.out",
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: trigEl,
+          start: "top center",
+        },
+      }
+    );
+  }, [arr, trig]);
+};
+
+export const useProjectLines = (arr, trig) => {
+  useEffect(() => {
+    const el = arr;
+    const trigEl = trig.current;
+
+    gsap.fromTo(
+      el,
+      {
+        width: 0,
+      },
+      {
+        width: "100%",
+        duration: 2,
+        ease: "power4.out",
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: trigEl,
+          start: "top center",
+        },
+      }
+    );
+  }, [arr, trig]);
+};
+
+export const useProjectTexts = (arr, trig, delay = 0.5) => {
+  useEffect(() => {
+    const el = arr;
+    const trigEl = trig.current;
+
+    gsap.fromTo(
+      el,
+      {
+        y: 500,
+      },
+      {
+        y: 0,
+        duration: 1,
+        delay: delay,
+        ease: "power4.out",
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: trigEl,
+          start: "top center",
+        },
+      }
+    );
+  }, [arr, trig, delay]);
+};
+
+export const useProjectsNumbers = (arr, trig) => {
+  useEffect(() => {
+    const el = arr;
+    const trigEl = trig.current;
+
+    gsap.fromTo(
+      el,
+      {
+        y: -500,
+      },
+      {
+        y: 0,
+        duration: 1,
+        delay: 1,
+        ease: "power4.out",
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: trigEl,
+          start: "top center",
+        },
+      }
+    );
+  }, [arr, trig]);
 };
